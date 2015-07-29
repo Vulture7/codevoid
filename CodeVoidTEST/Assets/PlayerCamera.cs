0fa212ce6548a02f169bbf6cd9b49d31f03a4c70
Xiode <Xiodetheone@gmail.com>
2015-07-29 02:48:36 -0600

    Initial Unity Project / Assets
    Added basic unity project and raw assets. - Thomas H

---
+++ CodeVoidTEST/Assets/PlayerCamera.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* PlayerCamera.cs
+ * Written by Thomas Hall, (C)2015
+ */
+
+public class PlayerCamera : MonoBehaviour {
+	
+	public float YSensitivity = 1.0f;
+	public float XSensitivity = 1.0f;
+
+	public bool ModifyVertical = true;
+	public bool ModifyHorizontal = false;
+
+	public bool Invert = false;
+	
+	float mousey, mousex;
+	
+	// Use this for initialization
+	void Start () {
+		mousey = Input.mousePosition.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (ModifyHorizontal) 
+			transform.Rotate (0f, -(mousex - Input.mousePosition.x) * XSensitivity, 0f);
+
+		if (ModifyVertical)
+			if ((mousey - Input.mousePosition.y) < 0 || transform.localRotation.x < 0.7) {
+				if ((mousey - Input.mousePosition.y) > 0 || transform.localRotation.x > -0.7) {
+					if (Invert) {
+						transform.Rotate (-(mousey - Input.mousePosition.y) * YSensitivity, 0f, 0f);
+					} else {
+						transform.Rotate ((mousey - Input.mousePosition.y) * YSensitivity, 0f, 0f);
+					}
+				} else {
+					transform.localRotation.Set(-0.7f,
+					                       transform.localRotation.y,
+					                       transform.localRotation.z,
+					                       transform.localRotation.w);
+				}
+			} else {
+				transform.localRotation.Set(0.7f,
+				                       transform.localRotation.y,
+				                       transform.localRotation.z,
+				                       transform.localRotation.w);
+			}
+		
+		mousex = Input.mousePosition.x;
+		mousey = Input.mousePosition.y;
+	}
+}
---
+++ CodeVoidTEST/Assets/PlayerControl.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public RuntimeAnimatorController rac;
+	Animator an;
+	public GameObject LookReference;
+	public GameObject TurnReference;
+	CharacterController cc;
+
+	AudioSource Gunshot;
+	public ParticleSystem GunFlare;
+
+	bool UpdateRotation = false;
+	bool AlreadyUpdating = false;
+	float ShootingTimer = 0f;
+
+	public float MoveSpeed;
+	public float AimMoveSpeed;
+	Quaternion LastTurn;
+
+	// Use this for initialization
+	void Start () {
+		an = GetComponent <Animator> ();
+
+		cc = GetComponent <CharacterController> ();
+
+		Gunshot = GetComponent <AudioSource>();
+	}
+
+	Vector3 movement = Vector3.zero;
+
+	// Update is called once per frame
+	void Update () {
+		UpdateRotation = false;
+
+		if (!cc.isGrounded)
+			movement.y -= Time.deltaTime;
+		else
+			movement.y = 0f;
+
+		if (Input.GetKey (KeyCode.W)) {
+			if (an.GetBool("Shooting"))
+				transform.position += transform.forward * Time.deltaTime * AimMoveSpeed;
+			else
+				transform.position += transform.forward * Time.deltaTime * MoveSpeed;
+
+			UpdateRotation = true;
+			an.SetBool ("RunForward", true);
+		}
+		if (!Input.GetKey (KeyCode.W) && an.GetBool ("RunForward")) {
+			an.SetBool ("RunForward", false);
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			UpdateRotation = true;
+			GunFlare.Play();
+			Gunshot.Play();
+			an.SetTrigger("Shoot");
+			an.SetBool ("Shooting", true);
+			ShootingTimer = 0.5f;
+		}
+		if (ShootingTimer > 0) {
+			ShootingTimer -= Time.deltaTime;
+		}
+		if (an.GetBool ("Shooting") && ShootingTimer < 0)
+			an.SetBool ("Shooting", false);
+
+		if (UpdateRotation) {
+			transform.rotation = TurnReference.transform.rotation;
+			TurnReference.transform.rotation = new Quaternion (0f, 0f, 0f, 0f);
+		} else
+			AlreadyUpdating = false;
+
+		cc.Move(movement);
+	}
+}
+
