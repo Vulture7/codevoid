068a3212d86f6536b708a88f967a5a2ece687256
Xiode <Xiodetheone@gmail.com>
2015-07-29 11:16:58 -0600

    Controls 2
    Bad commit last time.

--- CodeVoidTEST/Assets/PlayerControl.cs
+++ CodeVoidTEST/Assets/PlayerControl.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-	public RuntimeAnimatorController rac;
 	Animator an;
 	public GameObject LookReference;
 	public GameObject TurnReference;
+	public GameObject PlayerModel;
 	CharacterController cc;
 
 	AudioSource Gunshot;
@@ -22,7 +22,7 @@ public class PlayerControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		an = GetComponent <Animator> ();
+		an = PlayerModel.GetComponent <Animator> ();
 
 		cc = GetComponent <CharacterController> ();
 
@@ -40,18 +40,80 @@ public class PlayerControl : MonoBehaviour {
 		else
 			movement.y = 0f;
 
+		//Begin horrible, awful code that will be optimized soon
+
+		if (an.GetBool("Shooting"))
+		    PlayerModel.transform.rotation = Quaternion.LookRotation(transform.forward);
+
+		//Forward
 		if (Input.GetKey (KeyCode.W)) {
-			if (an.GetBool("Shooting"))
+			if (an.GetBool("Shooting")) {
 				transform.position += transform.forward * Time.deltaTime * AimMoveSpeed;
+			}
 			else
 				transform.position += transform.forward * Time.deltaTime * MoveSpeed;
-
+			
+			PlayerModel.transform.rotation = Quaternion.LookRotation(transform.forward);
+			
 			UpdateRotation = true;
 			an.SetBool ("RunForward", true);
 		}
 		if (!Input.GetKey (KeyCode.W) && an.GetBool ("RunForward")) {
 			an.SetBool ("RunForward", false);
 		}
+
+		//Left straft
+		if (Input.GetKey (KeyCode.A)) {
+			if (an.GetBool("Shooting")){
+				an.SetBool ("StrafeLeft", true);
+				transform.position -= transform.right * Time.deltaTime * AimMoveSpeed;
+			} else {
+				an.SetBool ("RunForward", true);
+				transform.position -= transform.right * Time.deltaTime * MoveSpeed;
+				PlayerModel.transform.rotation = Quaternion.LookRotation(-transform.right);
+			}
+			
+			UpdateRotation = true;
+		}
+		if (!Input.GetKey (KeyCode.D) && an.GetBool ("RunForward")) {
+			an.SetBool ("StrafeRight", false);
+		}
+
+		//Right strafe
+		if (Input.GetKey (KeyCode.D)) {
+			if (an.GetBool("Shooting")){
+				an.SetBool ("StrafeRight", true);
+				transform.position += transform.right * Time.deltaTime * AimMoveSpeed;
+			} else {
+				PlayerModel.transform.rotation = Quaternion.LookRotation(transform.right);
+				transform.position += transform.right * Time.deltaTime * MoveSpeed;
+				an.SetBool ("RunForward", true);
+			}
+			
+			UpdateRotation = true;
+		}
+		if (!Input.GetKey (KeyCode.D) && an.GetBool ("RunForward")) {
+			an.SetBool ("StrafeRight", false);
+		}
+		
+		//backwards
+		if (Input.GetKey (KeyCode.S)) {
+			if (an.GetBool("Shooting")){
+				an.SetBool ("WalkBackward", true);
+				transform.position -= transform.forward * Time.deltaTime * AimMoveSpeed;
+			} else {
+				PlayerModel.transform.rotation = Quaternion.LookRotation(-transform.forward);
+				an.SetBool ("RunForward", true);
+				transform.position -= transform.forward * Time.deltaTime * MoveSpeed;
+			}
+
+			UpdateRotation = true;
+		}
+		if (!Input.GetKey (KeyCode.W) && an.GetBool ("RunForward")) {
+			an.SetBool ("RunBackward", false);
+			an.SetBool ("WalkBackward", false);
+		}
+		//End horrible, awful code
 
 		if (Input.GetMouseButtonDown (0)) {
 			UpdateRotation = true;
@@ -60,6 +122,7 @@ public class PlayerControl : MonoBehaviour {
 			an.SetTrigger("Shoot");
 			an.SetBool ("Shooting", true);
 			ShootingTimer = 0.5f;
+			PlayerModel.transform.rotation = Quaternion.LookRotation(transform.forward);
 		}
 		if (ShootingTimer > 0) {
 			ShootingTimer -= Time.deltaTime;
