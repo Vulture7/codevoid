c9af1f2b7dec272d5323b2e088497fa8f8fe200f
Jake Cross <Stackoverload.cross@Gmail.com>
2015-07-30 08:49:18 -0700

    Added manager UI (Press E) and basic settings class

---
+++ CodeVoidTEST/Assets/Scripts/C#/Character/Player/UIController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+    public GameObject manager;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(SettingValues.managerKey))
+        {
+            manager.SetActive(!manager.activeSelf);
+        }
+    }
+
+
+}
